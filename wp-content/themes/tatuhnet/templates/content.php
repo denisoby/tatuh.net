f5ab37bd507d5aa9db53b14318245f0f82fae068
freevatar <itea@packer-debian-8-amd64.digitalocean.com>
2015-05-15 15:43:23 +0300

    Initial

---
+++ wp-content/themes/tatuhnet/templates/content.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Main content file.
+ */
+?>
+
+
+<article <?php post_class(); ?>>
+
+  <!-- removed post icons -->
+  <?php // get_template_part('templates/post-format-icons'); ?>
+
+  <!-- entry title -->
+  <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+  <!-- entry header hook -->
+  <?php do_action( 'daisyblue_entry_header' ); ?>
+
+  <div class="entry-content">
+    <!-- featured image -->
+    <?php if ( '' != get_the_post_thumbnail() ) { ?>
+      <div class="featured-image mask-wrap thumbnail pull-left">
+          <?php the_post_thumbnail('featured-blog'); ?>
+
+          <div class="mask">
+             <i class="fa fa-plus"></i>
+          </div>
+      </div>
+    <?php } ?>
+
+    <!-- entry summary -->
+    <div class="entry-summary">
+      <?php the_excerpt(); ?>
+    </div>
+  </div> <!-- .entry-content -->
+
+  <!-- entry footer hook -->
+  <?php do_action( 'daisyblue_entry_footer' ); ?>
+
+</article>
---
+++ wp-content/themes/tatuhnet/templates/page-header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * all page titles
+ */
+?>
+
+
+<div class="page-header">
+  <h1 class="entry-title">
+    <?php echo daisyblue_title(); ?>
+  </h1>
+</div>
