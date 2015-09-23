c0de87093b2695552e8ea3e359821257c015c76f
freevatar <freevatar@gmail.com>
2015-09-24 00:23:47 +0300

    Delete trash from header

--- wp-content/themes/tatuh.net/functions.php
+++ wp-content/themes/tatuh.net/functions.php
@@ -52,8 +52,13 @@ if (!function_exists('semplicemente_setup')) :
     {
 
         remove_action('wp_head', 'rsd_link');
-        remove_action('wp_head', 'wlwmanifest_link');
         remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'rel_canonical');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'feed_links', 2);
+	remove_action('wp_head', 'feed_links_extra', 3);
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
         /**
          * Set the content width based on the theme's design and stylesheet.
