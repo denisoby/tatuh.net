052cd4b58c7b8d717e1ec31b00bf56e51addafd8
freevatar <freevatar@gmail.com>
2015-09-29 17:30:39 +0300

    Sitemap plugin

--- wp-content/themes/tatuh.net/content.php
+++ wp-content/themes/tatuh.net/content.php
@@ -5,8 +5,8 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-	</header><!-- .entry-header -->
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header>
 <div class="thumbnail thumbnail-left">
 <?php
 if ( '' != get_the_post_thumbnail() ) {
@@ -17,13 +17,7 @@ echo '</a>';
 ?>
 </div>
 	<div class="entry-content">
-		<?php the_excerpt(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'semplicemente' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_excerpt(); ?>		
 		<a class="readMoreLink" href="<?php echo get_permalink(); ?>"><?php _e('Читать дальше', 'semplicemente') ?><i class="fa spaceLeft fa-angle-double-right"></i></a>
 </div><!-- .entry-content -->
 </article><!-- #post-## -->
